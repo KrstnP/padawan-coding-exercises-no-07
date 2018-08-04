0db1cb4ae769bc91128ed6607695d33eaf05ef74
Tin <krstnpangan@gmail.com>
2018-08-04 21:17:36 +0800

    Added conversion

--- converter.rb
+++ converter.rb
@@ -11,7 +11,28 @@ class Converter
     puts "Hello World"
     begin
       choice = menu
-      puts choice
+      if choice.downcase == "a"
+        puts ""
+        puts "Convert Celsius to Fahrenheit"
+        print "Celsius: "
+        celsius = gets.chomp.to_f
+        conversion = celsius * 1.8000 + 32.0
+        puts "Fahrenheit: #{conversion}"
+        puts ""
+
+      elsif choice.downcase == "b"
+        puts ""
+        puts "Convert Fahrenheit to Celsius"
+        print "Fahrenheit: "
+        fahrenheit = gets.chomp.to_f
+        conversion = (fahrenheit - 32.0) / 1.8000
+        puts "Celsius: #{conversion}"
+        puts ""
+
+      else
+        puts "It's not on the above menu"
+        puts ""
+      end
     end until choice == "x"
   end
 
@@ -19,7 +40,9 @@ class Converter
     puts "A. Convert Celsius to Fahrenheit"
     puts "B. Convert Fahrenheit to Celsius"
     puts "X. To Quit"
+    puts ""
     print "Enter your choice: "
     return gets.chomp.downcase
+
   end
 end
